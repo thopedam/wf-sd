aee33ada2bf382777dcf0af4a401277ab728fd11
Thorge Dammann <moin@thorge.dev>
2025-03-06 17:04:36 +0100

    adjusted in view for features

--- src/features.js
+++ src/features.js
@@ -19,35 +19,43 @@ const duration = 8;
 const tick = (element) => {
   clearTimeout(timeout);
   timeline?.totalProgress(1).kill();
-  timeline = gsap
-    .timeline()
-    .fromTo(
-      element.find(".bar"),
-      {
-        transformOrigin: "bottom left",
-        scaleX: 0,
-        scaleY: "100%",
-        background: "#6904C1",
-      },
-      {
-        scaleX: 1,
-        duration,
-        ease: "linear",
-      }
-    )
-    .to(element.find(".bar"), {
-      scaleY: "50%",
-      duration: 0.2,
-      background: "#A0A1AE",
-      ease: "power1.in",
-    });
-
+  const rect = element[0].getBoundingClientRect();
+  const isInView = rect.top >= 0 && rect.bottom <= window.innerHeight;
+  if (isInView) {
+    timeline = gsap
+      .timeline()
+      .fromTo(
+        element.find(".bar"),
+        {
+          transformOrigin: "bottom left",
+          scaleX: 0,
+          scaleY: "100%",
+          background: "#6904C1",
+        },
+        {
+          scaleX: 1,
+          duration,
+          ease: "linear",
+        }
+      )
+      .to(element.find(".bar"), {
+        scaleY: "50%",
+        duration: 0.2,
+        background: "#A0A1AE",
+        ease: "power1.in",
+      });
+  }
   timeout = setTimeout(() => {
     let next = element.next();
     if (element.next().length === 0) {
       next = $(barName).children().first();
     }
-    next.trigger("click");
+
+    if (isInView) {
+      next.trigger("click");
+    } else {
+      tick(element);
+    }
   }, duration * 1000);
 };
 
