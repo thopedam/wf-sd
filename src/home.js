701136e2aa1c2e8e7736a44e8659891116110f20
Thorge Dammann <moin@thorge.dev>
2025-04-07 15:14:26 +0200

    removed sticky cards animation

--- src/home.js
+++ src/home.js
@@ -9,75 +9,75 @@ const WINDOW_HEIGHT = window.innerHeight;
 const revalidateTimelines = [];
 let width = window.innerWidth;
 
-const staggerTL = gsap.timeline({
-  scrollTrigger: {
-    normalize: true,
-    trigger: ".sticky-cards-container",
-    scrub: 1,
-    start: "500px bottom",
-    end: "90% bottom",
-    toggleActions: "play reverse none none",
-  },
-});
-staggerTL.add("start", 0);
+// const staggerTL = gsap.timeline({
+//   scrollTrigger: {
+//     normalize: true,
+//     trigger: ".sticky-cards-container",
+//     scrub: 1,
+//     start: "500px bottom",
+//     end: "90% bottom",
+//     toggleActions: "play reverse none none",
+//   },
+// });
+// staggerTL.add("start", 0);
 
-staggerTL.fromTo(
-  ".sticky-card",
-  { scale: 1.5, xPercent: xStart, yPercent: -200, rotateZ: -45 },
-  {
-    scale: 1.0,
-    xPercent: positionX,
-    yPercent: positionY,
-    rotateZ: rotate,
-    stagger: 1.0,
-  },
-  "start"
-);
-revalidateTimelines.push(staggerTL);
+// staggerTL.fromTo(
+//   ".sticky-card",
+//   { scale: 1.5, xPercent: xStart, yPercent: -200, rotateZ: -45 },
+//   {
+//     scale: 1.0,
+//     xPercent: positionX,
+//     yPercent: positionY,
+//     rotateZ: rotate,
+//     stagger: 1.0,
+//   },
+//   "start"
+// );
+// revalidateTimelines.push(staggerTL);
 
-const containerWidth = () =>
-  $(".sticky-cards-container").length
-    ? $(".sticky-cards-container").width()
-    : 0;
+// const containerWidth = () =>
+//   $(".sticky-cards-container").length
+//     ? $(".sticky-cards-container").width()
+//     : 0;
 
-const containerPercentage = () => (containerWidth() / MAX_WIDTH) * 80;
+// const containerPercentage = () => (containerWidth() / MAX_WIDTH) * 80;
 
-function getRandom(seed, max) {
-  const x = Math.sin(Math.sin(1 * seed * 2238));
-  return x * max;
-}
+// function getRandom(seed, max) {
+//   const x = Math.sin(Math.sin(1 * seed * 2238));
+//   return x * max;
+// }
 
-function xStart(i) {
-  return i % 2 === 0 ? 500 : -500;
-}
+// function xStart(i) {
+//   return i % 2 === 0 ? 500 : -500;
+// }
 
-function rotate(i) {
-  return getRandom(i * 2, 3);
-}
+// function rotate(i) {
+//   return getRandom(i * 2, 3);
+// }
 
-function positionX(i) {
-  if (window.innerWidth < MOBILE_BREAKPOINT) {
-    return 0; //getRandom(i * 5, 2);
-  }
-  let value =
-    (containerPercentage() -
-      (i % 3) * containerPercentage() +
-      getRandom(i * 3, 5)) *
-    -1;
-  if (window.innerWidth < TABLET_BREAKPOINT) {
-    value *= 0.5;
-  }
-  if (window.innerWidth > MAX_WIDTH) {
-    value *= window.innerWidth / MAX_WIDTH;
-  }
-  return value;
-}
-function positionY(i) {
-  if (window.innerWidth < MOBILE_BREAKPOINT) {
-    return i * 2;
-  }
-  return Math.floor(i / 3) * 100 - 50 + getRandom(i / 12, -20);
-}
+// function positionX(i) {
+//   if (window.innerWidth < MOBILE_BREAKPOINT) {
+//     return 0; //getRandom(i * 5, 2);
+//   }
+//   let value =
+//     (containerPercentage() -
+//       (i % 3) * containerPercentage() +
+//       getRandom(i * 3, 5)) *
+//     -1;
+//   if (window.innerWidth < TABLET_BREAKPOINT) {
+//     value *= 0.5;
+//   }
+//   if (window.innerWidth > MAX_WIDTH) {
+//     value *= window.innerWidth / MAX_WIDTH;
+//   }
+//   return value;
+// }
+// function positionY(i) {
+//   if (window.innerWidth < MOBILE_BREAKPOINT) {
+//     return i * 2;
+//   }
+//   return Math.floor(i / 3) * 100 - 50 + getRandom(i / 12, -20);
+// }
 
 /* -------------------------------------------------------------------------- */
 /*                    Still fighting your CRM, not selling?                   */
